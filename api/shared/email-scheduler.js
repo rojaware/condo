b9dcd610764e17f4665df8d2421a145a558f6fdd
SCOTT-YOGA\Admin <leesungki@gmail.com>
2024-04-05 16:54:18 -0400

    added comment

--- api/shared/email-scheduler.js
+++ api/shared/email-scheduler.js
@@ -26,7 +26,7 @@ function startCron() {
       console.log("no data...");    
     } else {
       console.log(`Run email job at ${alert.hour}:${alert.minute} every day.`); 
-      // cron.schedule(`* * * * *`, () => {
+      // cron.schedule(`* * * * *`, () => { // run every minute..
       cron.schedule(`${alert.minute} ${alert.hour} * * *`, () => {  
         // Send e-mail if today is 70 days away toward lease end date
         sendMail(alert);
